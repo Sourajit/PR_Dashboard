9712a69a22942264cebf124e7c985c6bca9dc2ba
Pragmatic 5 <91sourajit@gmail.com>
2021-05-30 19:54:32 +0530

    initial commit for develop

--- src/components/TopMenu/MyMenu.js
+++ src/components/TopMenu/MyMenu.js
@@ -19,8 +19,6 @@ const options = [
   },
   { key: 'profile', text: 'Your Profile' },
   { key: 'stars', text: 'Your Stars' },
-  { key: 'explore', text: 'Explore' },
-  { key: 'integrations', text: 'Integrations' },
   { key: 'help', text: 'Help' },
   { key: 'settings', text: 'Settings' },
   { key: 'sign-out', text: 'Sign Out' }
